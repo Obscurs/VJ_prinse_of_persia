faf909a21d39a85fc297d7e607c6460b158e409b
Obscurs <ferris202@gmail.com>
2017-03-04 01:25:11 +0100

    new project

---
+++ 2DGame/02-Bubble/02-Bubble/Scene.cpp
@@ -0,0 +1,114 @@
+#include <iostream>
+#include <cmath>
+#include <glm/gtc/matrix_transform.hpp>
+#include <Windows.h>
+#include <iostream>
+#include <sstream>
+#include "Scene.h"
+#include "Game.h"
+
+
+#define SCREEN_X 0
+#define SCREEN_Y 0
+
+#define CHUNK_X_SIZE 256
+#define CHUNK_Y_SIZE 256
+#define INIT_PLAYER_X_TILES 4
+#define INIT_PLAYER_Y_TILES 25
+
+#define DBOUT( s )            \
+{                             \
+   std::wostringstream os_;    \
+   os_ << s;                   \
+   OutputDebugStringW( os_.str().c_str() );  \
+}
+
+Scene::Scene()
+{
+	map = NULL;
+	player = NULL;
+}
+
+Scene::~Scene()
+{
+	if(map != NULL)
+		delete map;
+	if(player != NULL)
+		delete player;
+}
+
+
+void Scene::init()
+{
+	initShaders();
+	map = TileMap::createTileMap("levels/level01.txt", glm::vec2(SCREEN_X, SCREEN_Y), texProgram);
+	player = new Player();
+	player->init(glm::ivec2(SCREEN_X, SCREEN_Y), texProgram);
+	player->setPosition(glm::vec2(INIT_PLAYER_X_TILES * map->getTileSize(), INIT_PLAYER_Y_TILES * map->getTileSize()));
+	player->setTileMap(map);
+	projection = glm::ortho(0.f, float(SCREEN_WIDTH - 1), float(SCREEN_HEIGHT - 1), 0.f);
+	currentTime = 0.0f;
+}
+
+void Scene::update(int deltaTime)
+{
+	currentTime += deltaTime;
+	player->update(deltaTime);
+
+	
+	int x = player->getPosition().x + 16;
+	int y = player->getPosition().y + 16;
+	int chunk_x = x / (CHUNK_X_SIZE);
+	int chunk_y = y / (CHUNK_Y_SIZE);
+	x = chunk_x*(CHUNK_X_SIZE);
+	y = chunk_y*(CHUNK_Y_SIZE);
+	DBOUT("positionX: " << x << "\n");
+	projection = glm::ortho((float)x, x + float(CHUNK_X_SIZE), y + float(CHUNK_Y_SIZE), (float)y);
+}
+
+void Scene::render()
+{
+	glm::mat4 modelview;
+
+	texProgram.use();
+	texProgram.setUniformMatrix4f("projection", projection);
+	texProgram.setUniform4f("color", 1.0f, 1.0f, 1.0f, 1.0f);
+	modelview = glm::mat4(1.0f);
+	texProgram.setUniformMatrix4f("modelview", modelview);
+	texProgram.setUniform2f("texCoordDispl", 0.f, 0.f);
+	map->render();
+	player->render();
+}
+
+void Scene::initShaders()
+{
+	Shader vShader, fShader;
+
+	vShader.initFromFile(VERTEX_SHADER, "shaders/texture.vert");
+	if(!vShader.isCompiled())
+	{
+		cout << "Vertex Shader Error" << endl;
+		cout << "" << vShader.log() << endl << endl;
+	}
+	fShader.initFromFile(FRAGMENT_SHADER, "shaders/texture.frag");
+	if(!fShader.isCompiled())
+	{
+		cout << "Fragment Shader Error" << endl;
+		cout << "" << fShader.log() << endl << endl;
+	}
+	texProgram.init();
+	texProgram.addShader(vShader);
+	texProgram.addShader(fShader);
+	texProgram.link();
+	if(!texProgram.isLinked())
+	{
+		cout << "Shader Linking Error" << endl;
+		cout << "" << texProgram.log() << endl << endl;
+	}
+	texProgram.bindFragmentOutput("outColor");
+	vShader.free();
+	fShader.free();
+}
+
+
+
