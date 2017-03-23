5b0ac47c663945083a73a4b83cf6cb19b5bf9921
Obscurs <ferris202@gmail.com>
2017-03-23 16:55:23 +0100

    changed background sprites

--- 2DGame/02-Bubble/02-Bubble/TileMap.cpp
+++ 2DGame/02-Bubble/02-Bubble/TileMap.cpp
@@ -83,11 +83,16 @@ bool TileMap::loadLevel(const string &levelFile)
 		for(int i=0; i<mapSize.x; i++)
 		{
 			fin.get(tile);
-			if(tile == ' ')
-				map[j*mapSize.x+i] = 0;
-			else
-				map[j*mapSize.x+i] = tile - int('0');
+			if (tile == ' ' || tile == '0'){
+				map[j*mapSize.x + i] = 0;
+				//std::cout << " ";
+			}
+			else{
+				map[j*mapSize.x + i] = tile - int('a')+1;
+				//std::cout << tile - int('a')+1;
+			}
 		}
+		//std::cout << std::endl;
 		fin.get(tile);
 #ifndef _WIN32
 		fin.get(tile);
@@ -115,7 +120,7 @@ void TileMap::prepareArrays(const glm::vec2 &minCoords, ShaderProgram &program)
 				// Non-empty tile
 				nTiles++;
 				posTile = glm::vec2(minCoords.x + i * tileSize, minCoords.y + j * tileSize);
-				texCoordTile[0] = glm::vec2(float((tile-1)%4) / tilesheetSize.x, float((tile-1)/4) / tilesheetSize.y);
+				texCoordTile[0] = glm::vec2(float((tile-1)%8) / tilesheetSize.x, float((tile-1)/8) / tilesheetSize.y);
 				texCoordTile[1] = texCoordTile[0] + tileTexSize;
 				//texCoordTile[0] += halfTexel;
 				texCoordTile[1] -= halfTexel;
