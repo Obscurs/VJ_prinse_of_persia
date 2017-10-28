6b484a1f52ff9e5f51448e07d87ee285f54d55f4
Genís Bayona <genis.bayona@est.fib.upc.edu>
2017-10-28 17:06:16 +0200

    mostly cleaning code
    : )

--- 2DGame/02-Bubble/02-Bubble/main.cpp
+++ 2DGame/02-Bubble/02-Bubble/main.cpp
@@ -5,79 +5,66 @@
 #include <ctime>
 #include "Game.h"
 
-
 //Remove console (only works in Visual Studio)
 //#pragma comment(linker, "/subsystem:\"windows\" /entry:\"mainCRTStartup\"")
 
-
 #define TIME_PER_FRAME 1000.f / 60.f // Approx. 60 fps
 
-
+static Game game; // This object represents the whole game
 static int prevTime;
-static Game game; // This object represents our whole game
 
-
-// If a key is pressed this callback is called
-
+// If a key is pressed this callback will be called
 static void keyboardDownCallback(unsigned char key, int x, int y)
 {
-	Game::instance().keyPressed(key);
+    Game::instance().keyPressed(key);
 }
 
-// If a key is released this callback is called
-
+// If a key is released this callback will be called
 static void keyboardUpCallback(unsigned char key, int x, int y)
 {
-	Game::instance().keyReleased(key);
+    Game::instance().keyReleased(key);
 }
 
-// If a special key is pressed this callback is called
-
+// If a special key is pressed this callback will be called
 static void specialDownCallback(int key, int x, int y)
 {
-	Game::instance().specialKeyPressed(key);
+    Game::instance().specialKeyPressed(key);
 }
 
-// If a special key is released this callback is called
-
+// If a special key is released this callback will be called
 static void specialUpCallback(int key, int x, int y)
 {
-	Game::instance().specialKeyReleased(key);
+    Game::instance().specialKeyReleased(key);
 }
 
 // Same for changes in mouse cursor position
-
 static void motionCallback(int x, int y)
 {
-	Game::instance().mouseMove(x, y);
+    Game::instance().mouseMove(x, y);
 }
 
 // Same for mouse button presses or releases
-
 static void mouseCallback(int button, int state, int x, int y)
 {
-	if(state == GLUT_DOWN)
-		Game::instance().mousePress(button);
-	else if(state == GLUT_UP)
-		Game::instance().mouseRelease(button);
+    if(state == GLUT_DOWN) Game::instance().mousePress(button);
+    else if(state == GLUT_UP) Game::instance().mouseRelease(button);
 }
 
 static void drawCallback()
 {
-	Game::instance().render();
-	glutSwapBuffers();
+    Game::instance().render();
+    glutSwapBuffers();
 }
 
 static void idleCallback()
 {
-	int currentTime = glutGet(GLUT_ELAPSED_TIME);
-	int deltaTime = currentTime - prevTime;
+    int currentTime = glutGet(GLUT_ELAPSED_TIME);
+    float deltaTime = currentTime - prevTime;
 	
 	if(deltaTime > TIME_PER_FRAME)
 	{
-		// Every time we enter here is equivalent to a game loop execution
-		if (!Game::instance().update(deltaTime))
-			exit(0);
+		// Every time we enter here is the start of a game loop execution
+		if (!Game::instance().update(deltaTime)) exit(0);
 		prevTime = currentTime;
 		glutPostRedisplay();
 	}
@@ -111,13 +98,8 @@ int main(int argc, char **argv)
 	Game::instance().init();
 	prevTime = glutGet(GLUT_ELAPSED_TIME);
 	// GLUT gains control of the application
-	//PlaySound(TEXT("sounds/test.wav"), NULL, SND_ASYNC);
 	
-
 	glutMainLoop();
 
 	return 0;
 }
-
-
-
